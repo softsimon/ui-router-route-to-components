7a5bd9815e9e8c8e5f8fde7c7476686d257c3f4c
Simon Lindh <simonlindh.se@gmail.com>
2016-04-24 02:40:09 +0200

    Indentation

--- routeToComponents.js
+++ routeToComponents.js
@@ -2,8 +2,8 @@
 	'use strict';
 
 	angular
-		.module('ui.router.components', ['ui.router'])
-		.config(registerComponentDecorator);
+	.module('ui.router.components', ['ui.router'])
+	.config(registerComponentDecorator);
 
 	var args2Array = function args2Array(_args) {
 		return Array.prototype.slice.call(_args);
@@ -13,10 +13,10 @@
 		var injectNames = controller.$inject = ['$scope'].concat(args2Array(arguments));
 
 		function controller($scope) {
-				var injectValues = args2Array(arguments);
-				for (var i = 1; i < injectValues.length; i++) {
-						$scope[injectNames[i]] = injectValues[i];
-				}
+			var injectValues = args2Array(arguments);
+			for (var i = 1; i < injectValues.length; i++) {
+				$scope[injectNames[i]] = injectValues[i];
+			}
 		}
 
 		return controller;
@@ -24,7 +24,7 @@
 
 	var getCompInputs = function getCompInputs($injector, componentName) {
 		return $injector.get(componentName + "Directive").map(function (directive) {
-				return Object.keys(directive.bindToController);
+			return Object.keys(directive.bindToController);
 		}).reduce(unnestR, []);
 	};
 
@@ -34,35 +34,35 @@
 
 	function setStateDef(stateDef) {
 		stateDef.controllerProvider = function ($injector) {
-				return bindResolves.apply(null, getCompInputs($injector, stateDef.component));
+			return bindResolves.apply(null, getCompInputs($injector, stateDef.component));
 		};
 
 		stateDef.templateProvider = function ($injector) {
-				var attrs = getCompInputs($injector, stateDef.component).map(function (key) {
-						return key + '="' + key + '"';
-				}).join(" ");
-				var kebobName = stateDef.component.replace(/([A-Z])/g, function ($1) {
-						return "-" + $1.toLowerCase();
-				});
-				var template = '<' + kebobName + ' ' + attrs + '></' + kebobName + '>';
-				return template;
+			var attrs = getCompInputs($injector, stateDef.component).map(function (key) {
+				return key + '="' + key + '"';
+			}).join(" ");
+			var kebobName = stateDef.component.replace(/([A-Z])/g, function ($1) {
+				return "-" + $1.toLowerCase();
+			});
+			var template = '<' + kebobName + ' ' + attrs + '></' + kebobName + '>';
+			return template;
 		};
 	}
 
 	function registerComponentDecorator($stateProvider) {
 		$stateProvider.decorator('component', function (stateDef, parent) {
-				if (stateDef.component) {
-						setStateDef(stateDef);
+			if (stateDef.component) {
+				setStateDef(stateDef);
+			}
+			if (stateDef.views) {
+				for (var view in stateDef.views) {
+					if (stateDef.views[view].component) {
+						setStateDef(stateDef.views[view]);
+					}
 				}
-				if (stateDef.views) {
-						for (var view in stateDef.views) {
-								if (stateDef.views[view].component) {
-										setStateDef(stateDef.views[view]);
-								}
-						}
-				}
+			}
 
-				return stateDef.component;
+			return stateDef.component;
 		});
 	}
 
